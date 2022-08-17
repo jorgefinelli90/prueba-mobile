7869b0df222faf053d49ed1f4118e36c9499671e
Jorge Finelli <jorgefinelli90@gmail.com>
2022-08-17 18:51:29 -0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+/* menu responsive */
+const ham = document.querySelector('.ham');
+const enlaces = document.querySelector('.enlaces-menu');
+const barras = document.querySelectorAll('.ham span');
+
+
+
+ham.addEventListener('click', () => {
+    enlaces.classList.toggle('activado');
+    barras.forEach(child => {child.classList.toggle('animado')});
+    ham.classList.toggle('girar');
+});
