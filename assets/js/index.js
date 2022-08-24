5ca69e76e2398212b84f21c4d0fd18e0a4bf0835
Jorge Finelli <jorgefinelli90@gmail.com>
2022-08-23 21:41:13 -0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,13 @@
 const ham = document.querySelector('.ham');
 const enlaces = document.querySelector('.enlaces-menu');
 const barras = document.querySelectorAll('.ham span');
+const nav = document.querySelector('.navR')
 
 
 
 ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
+    nav.classList.toggle('mover')
     barras.forEach(child => {child.classList.toggle('animado')});
     ham.classList.toggle('girar');
 });
